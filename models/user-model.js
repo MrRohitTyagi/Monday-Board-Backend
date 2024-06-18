2547f46d83e1c8b818b3454eb0bc53dd213e4f5b
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-06-18 23:56:54 +0530

    user password reset fix

--- models/user-model.js
+++ models/user-model.js
@@ -20,9 +20,11 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword;
+    if (this.isNew) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+    }
 
     next();
   } catch (error) {
