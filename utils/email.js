80e334b616e157a1e2609bde5204a19f059f5691
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-06-20 23:00:27 +0530

    email creds to .env

--- utils/email.js
+++ utils/email.js
@@ -1,12 +1,17 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env.local" });
+
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
 
 export const sendMail = async ({ to, subject, text, html }) => {
   try {
     let transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "rt2tyagi4366@gmail.com",
-        pass: "zxgwqvztssxfjlmu",
+        user: EMAIL,
+        pass: PASSWORD,
       },
     });
 
