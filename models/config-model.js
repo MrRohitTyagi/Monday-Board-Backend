4c4d6803806d73e5e0202fda5c1f087f6533cefa
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-06-27 21:10:38 +0530

    3 filters added

--- models/config-model.js
+++ models/config-model.js
@@ -2,25 +2,14 @@ import mongoose from "mongoose";
 
 const userConfigSchema = mongoose.Schema(
   {
-    belongsTo: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-    },
-    staredBoards: [
-      {
-        type: String,
-      },
-    ],
-    likedChats: [
-      {
-        type: String,
-      },
-    ],
-    likedThreads: [
-      {
-        type: String,
-      },
-    ],
+    belongsTo: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    staredBoards: [{ type: String }],
+    likedChats: [{ type: String }],
+    likedThreads: [{ type: String }],
+    search: { type: String },
+    priority: { type: String },
+    status: { type: String },
+    user: { type: String },
   },
   { versionKey: false }
 );
